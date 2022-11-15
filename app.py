db57970956d2f76cc09a233e392e4d517b9dd50d
ricky <ricky.ilinke@gmail.com>
2022-11-15 21:59:25 +0800

    n

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import *
+from repository.attractionRepository import AttractionRepository
+
+ar =  AttractionRepository()
+
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
@@ -17,7 +21,37 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
+#Api 旅遊景點
+@app.route("/api/attractions")
+def apiAttractions():
+	page = int(request.args.get('page'))
+	keyword = request.args.get('keyword')
+	attractions = ar.getAttractionsByPageAndKeyword(page, keyword )
+	data = {
+		"nextPage":page + 1,
+		"data":	attractions
+		}
+	return data
+@app.route("/api/attraction/<attractionId>")
+def apiAttractionByAttractionId(attractionId):
+	return
 
 
-app.run(port=3000)
+#Api 旅遊景點分類
+@app.route("/api/categories")
+def apiCategories():
+	return
 
+
+
+attraction = ar.getAttractionsById(1)
+
+attractions = ar.getAttractionsByPageAndKeyword()
+
+
+print(attractions)
+
+if __name__ == "__main__":
+    app.run(port=3000, debug = True) 
+
+ 
