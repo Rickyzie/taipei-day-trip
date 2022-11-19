ac282242027ef902e4fcd088976689527c82a4a7
Ricky <a0935640996@gmail.com>
2022-11-19 10:36:04 +0800

    n

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def apiCategories():
 		return jsonify({"error": True,"message": "server error"}), 500
 
 if __name__ == "__main__":
-    app.run(port=3000, debug = True)
+    app.run(host="0.0.0.0", port=3000, debug = True)
     app.config["JSON_SORT_KEYS"] = False
 
  
