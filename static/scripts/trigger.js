dfd4924579f825c9228fbd09403ac3ce18e11e78
ricky <ricky.ilinke@gmail.com>
2022-11-25 20:25:46 +0800

    correct css layout

--- static/scripts/trigger.js
+++ static/scripts/trigger.js
@@ -8,31 +8,12 @@ export default class Trigger {
         this.scrollLoader = document.querySelector(scrollLoader);
         this.callback = callback;
         this.observer = new IntersectionObserver((entries)=>{
-            if (entries[0].isIntersecting) this.callback()
+            if (entries[0].isIntersecting) this.callback();
         }, this.option);
         if(this.scrollLoader) this.observer.observe(this.scrollLoader);
     }
 }
 
-// let counter = 1;
-// let canScroll = false;
-// let nextPage = null;
-// getData(0)
-// const ScrollLoader = document.querySelector('.ScrollLoader')
-
-// const option = {
-//     root: null,
-//     rootMargin: "20px",
-//     threshold: 0
-// };
-// const handleObserver = (entries) => {
-//     const target = entries[0];
-//     if (target.isIntersecting && canScroll && nextPage) {
-//         getData(nextPage)
-//     }
-// };
-
-// const observer = new IntersectionObserver(handleObserver, option);
 
 
 
