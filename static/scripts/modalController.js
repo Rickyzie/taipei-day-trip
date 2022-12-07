32d959b87c81e62c7a6499cbf1f88e72a535cfc8
黃柏翔 <95478695+Rickyzie@users.noreply.github.com>
2022-12-07 14:42:50 +0800

    modal handler add

--- static/scripts/modalController.js
+++ static/scripts/modalController.js
@@ -3,13 +3,51 @@ class ModalController {
 
     }
 
-    CloseButtonHandler(){
-        const closeButton = document.querySelector('.CloseButton');
-        closeButton.addEventListener("click", ()=>{
-            closeButton.classList.toggle("show");
-        });
-
+    getLoginFormData(){
+        const loginForm = document.getElementById('LoginForm');
+        const loginFormData = new FormData(loginForm);
+        return loginFormData;
     }
 
+    getSignupFormData(){
+        const signupForm = document.getElementById('SignupForm');
+        const signupFormData = new FormData(signupForm);
+        return signupFormData;
+    }
 
+    ChangeModalToSignupHandler(){
+        const loginModalSignup = document.querySelector('.ChangeModalToSignup');
+        loginModalSignup.addEventListener("click", ()=>{
+            this.ChangeToggleBox()
+        })
+    }
+
+    ChangeModalToLoginHandler(){
+        const loginModalLogin = document.querySelector('.ChangeModalToLogin');
+        loginModalLogin.addEventListener("click", ()=>{
+            this.ChangeToggleBox()
+        })
+    }
+
+    CloseButtonHandler(){
+        const closeButton = document.querySelector('.Modal');
+        closeButton.addEventListener("click", ()=>{
+            document.querySelector('.Modal').classList.remove("show");
+            document.querySelector('.BlackCover').classList.remove("show");
+        });
+    }
+
+    ChangeToggleBox(){
+        const loginToggleBox = document.querySelector('.Login.ToggleBox');
+        const signupToggleBox = document.querySelector('.Siginup.ToggleBox')
+        
+        if (loginToggleBox.classList.contains("show")){
+            loginToggleBox.classlist.remove("show");
+            signupToggleBox.classlist.add("show");
+        }else{
+            loginToggleBox.classlist.add("show");
+            signupToggleBox.classlist.remove("show");
+        }
+    }
 }
+
